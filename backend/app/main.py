da7a24ac215905fc7608052b16cb67255cdefa34
ABRAHAMMFTS <abrahammarquezfuentes8@gmail.com>
2026-05-08 21:11:18 -0500

    correccion del dominio url para poder hacer inicios de sesion

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,11 +10,20 @@ app = FastAPI(title="SportPoint API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://sportpoint-jade.vercel.app","http://localhost:4200", "http://localhost:4201", "http://127.0.0.1:4200", "http://127.0.0.1:4201", "http://localhost:8000"],
+    allow_origins=[
+        "https://sportpoint-jade.vercel.app",
+        "https://sportpoint-1lwnolw8r-abrahammfts-projects.vercel.app",
+        "http://localhost:4200",
+        "http://localhost:4201",
+        "http://127.0.0.1:4200",
+        "http://127.0.0.1:4201",
+        "http://localhost:8000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(usuarios.router)
 app.include_router(deporte.router)
